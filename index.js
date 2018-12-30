552a46ebb45e0a584bd1bc4863f6bce4fb9cb020
Stanley1111 <stanley.tyl@gmail.com>
2018-12-30 17:15:05 +0000

    sum nested array complete

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  if (array.length > 1){
+    return sumItems(array.slice(0,1)) + sumItems(array.slice(1));
+  }
+  else if (Array.isArray(array[0])){
+    return sumItems(array[0]);
+  }
+  else {
+    return( array[0] ? array[0] : 0 );
+
+
+  }
 }
 
 module.exports = sumItems;
